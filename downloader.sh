d258eeedccb56c842a792788fe78b3c5dedab720
markus681 <62546076+markus681@users.noreply.github.com>
2020-05-19 08:46:54 +0200

    Update downloader.sh
    Incomplete, Show Menu again missing an other Stuff todo

--- downloader.sh
+++ downloader.sh
@@ -2,37 +2,107 @@
 # Programm für Youtube-DL zur einfachen benutzung zum Download
 # für Video und Audio als auswahlmöglichkeit
 
-
-# URL Eingabe
-echo "Enter URL"
-read url
-
-# Format abfrage
-youtube-dl -F $url > format
-
-i=1
-
-cut -d\  -f1 format | while read line
+# Auswahl für zusätzliche Optionen sowie Download
+PS3='Bitte auswahl treffen: '
+options=("Show Extractors" "ignore Config" "Set Filename" "Activate quiet mode" "Verbose Mode" "Display traffic" "Extract Audio" "Start Download" "Ende")
+select opt in "${options[@]}"
 do
-	echo "$i.) $line"
-	i=`expr $i + 1`
+	case $opt in
+		"Show Extractors")
+			youtube-dl --list-extractors > extractors
+			cat extractors
+			;;
+
+		"ignore Config")
+			iconfig=--ignore-config
+			;;
+
+		"Set Filename")
+			echo "Dateiname Eingeben: "
+			read setfile
+			;;
+
+		"Activate quite mode")
+			quite=--quite
+			;;
+
+		"Verbose Mode")
+			verbose=--verbose
+			;;
+
+		"Display traffic")
+			traffic=--print-traffic
+			;;
+
+		"Extract Audio")
+			echo "Specify audio format: best, aac, flac, mp3, m4a, opus, vorbis, or wav"
+			read aformat
+			eaudio=--extract-audio
+			audioo=--audio-format
+			;;
+
+		"Start Download")
+			# URL Eingabe
+			echo "Enter URL"
+			read url			
+			
+			# Direkt Downloaden oder Auswählen
+			PS3='Bitte auswahl treffen: '
+			auswahl=("Direkter Download" "Download mit Option" "Audio Only" "Video/Audio" "Video/Audio best" "Ende")
+			select aopt in "${auswahl[@]}"
+			do
+				case $aopt in
+					"Direkter Download")
+					youtube-dl $url
+					;;
+					
+					"Download mit Optionen")
+					youtube-dl $iconfig $setfile $quite $verbose $traffic $url
+					;;
+					
+					"Audio Only")
+					youtube-dl $eaudio $audioo $aformat $url
+					;;
+			
+					"Video/Audio")
+					# Format abfrage
+					youtube-dl -F $url > format
+
+					i=1
+
+					cat format
+
+#					cut -d\  -f1 format | while read line
+#					do
+#						echo "$i.) $line"
+#						i=`expr $i + 1`
+#					done
+					
+					echo "Video"
+					read video
+					echo "Audio"
+					read audio
+					echo $video+$audio
+					youtube-dl -f $video+$audio $url
+					;;
+					
+					"Video/Audio best")
+					youtube-dl -f best $url
+					;;
+					
+					"Ende")
+						break
+						;;
+						
+					*) echo "Ungültige auswahl $REPLY";;
+				esac
+			done
+			;;
+			
+		"Ende")
+			break
+			;;
+			
+		*) echo "Ungültige auswahl $REPLY";;
+	esac
 done
-
-# Auswahl treffen
-echo "Einfacher Download (1) oder Auswahl von AV (2)"
-read -n 1 antwort
-
-if [ "$antwort" = "1" ]
-then
-	youtube-dl $url
-fi
-
-if [ "$antwort" = "2" ]
-then 
-	echo "Auswahl von Video und Audio"
-	echo "Video Format"
-	read -n 2 cut -d\  -f1 $line > video
-	echo "Audio Format"
-	read -n 2 cut -d\  -f1 $line > audio
-	youtube-dl -f $video+$audio $url
-fi
